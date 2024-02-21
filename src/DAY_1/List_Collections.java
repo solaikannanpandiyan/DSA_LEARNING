6b31f989567fcf50ba39f196427a9f81e2bc1f46
solaikannan pandiyan <solaikannanpandiyan@gmail.com>
2024-02-21 15:17:18 +0530

    list collection + min_max

--- src/DAY_1/List_Collections.java
+++ src/DAY_1/List_Collections.java
@@ -27,26 +27,72 @@ public class List_Collections {
         //child container/variable = parent object - not allowed
 
         //add
+        System.out.println();
+        System.out.println("ADD");
         ls.add(1);
         ls.add(2);
         ls.add(3);
         System.out.println(ls);
 
         // Read
+        System.out.println();
         System.out.println("READ");
         System.out.println(ls.get(0));
         System.out.println(ls);
+
         // Update
-        ls.set(1,10);
+        System.out.println();
+        System.out.println("UPDATE");
+        ls.set(1,20);
         System.out.println(ls);
 
+        ls.add(2);
+
         // Delete
+        System.out.println();
+        System.out.println("DELETE");
+        ls.remove(2);
+        System.out.println(ls);
+        ls.remove(new Integer(1));
+        System.out.println(ls);
 
         // Iteration
+        System.out.println();
+        System.out.println("ITERATE");
+        ls.add(1);
+        ls.add(2);
+        for(int i :ls){
+            System.out.println(i);
+        }
+        System.out.println();
+
+        for(Integer i :ls){
+            System.out.println(i);
+        }
+        System.out.println();
+
+        for(int i = 0 ;i< ls.size();i++){
+            System.out.println(ls.get(i));
+        }
+        System.out.println();
+
+        Iterator<Integer> ix = ls.iterator();
+        while(ix.hasNext()) {
+            System.out.println(ix.next());
+        }
+
+        //
 
         // Search
+        System.out.println();
+        System.out.println("SEARCH");
+        System.out.println(ls.contains(20));
+        System.out.println(ls.indexOf(new Integer(20)));
+        System.out.println(ls.lastIndexOf(new Integer(20)));
 
         // Sort
+        System.out.println();
+        System.out.println("SORT");
 
     }
 
---
+++ src/DAY_1/problems/MinAndMax.java
@@ -0,0 +1,40 @@
+package DAY_1.problems;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class MinAndMax {
+
+    public static void iter(){
+        int Min = Integer.MAX_VALUE;
+        int Max = Integer.MIN_VALUE;
+
+        ArrayList<Integer> ax = new ArrayList<>();
+        ax.add(19);
+        ax.add(-3);
+        ax.add(7);
+        ax.add(23);
+        ax.add(5);
+        System.out.println(ax);
+        Iterator<Integer> itx = ax.iterator();
+        while(itx.hasNext()){
+            int x = itx.next();
+            if(x > Max){
+                Max = x;
+            }
+            if(x < Min){
+                Min = x;
+            }
+        }
+        System.out.println("MAX element: "+Max);
+        System.out.println("MAX element: "+Min);
+    }
+
+    public static void main(String[] args){
+        iter();
+
+
+    }
+
+
+    }
