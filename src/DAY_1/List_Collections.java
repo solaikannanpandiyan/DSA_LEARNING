297b53af653104a2eae5856d177f7fd086d7e47c
solaikannan pandiyan <solaikannanpandiyan@gmail.com>
2024-02-21 11:00:55 +0530

    resources

--- src/DAY_1/List_Collections.java
+++ src/DAY_1/List_Collections.java
@@ -6,5 +6,49 @@ import java.util.*;
 public class List_Collections {
 
 
+    public static void main(String[] args){
+        //static
+//        int[] arr = new int[10];
+//        int[] arr1 = {1,2,3};
+
+        //dynamic
+        // ArrayList<Integer> ls = new ArrayList<Integer>();
+
+        // Create
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        List<Integer> ls = new ArrayList<>();
+        Collection<Integer> cl = new ArrayList<>();
+
+//        ls = new Vector<Integer>();
+
+        //ArrayList = child
+        //List = parent
+        //parent container/variable = child object - allowed
+        //child container/variable = parent object - not allowed
+
+        //add
+        ls.add(1);
+        ls.add(2);
+        ls.add(3);
+        System.out.println(ls);
+
+        // Read
+        System.out.println("READ");
+        System.out.println(ls.get(0));
+        System.out.println(ls);
+        // Update
+        ls.set(1,10);
+        System.out.println(ls);
+
+        // Delete
+
+        // Iteration
+
+        // Search
+
+        // Sort
+
+    }
+
 
 }
--- src/DAY_1/wrapper_class.java
+++ src/DAY_1/wrapper_class.java
@@ -27,5 +27,6 @@ public class wrapper_class {
 
     public static void main(String[] args){
 
+
     }
 }
