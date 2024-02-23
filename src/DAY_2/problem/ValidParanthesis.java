34699e5f4a6b1ed504be9bf410f1e22d1de7d915
solaikannan pandiyan <solaikannanpandiyan@gmail.com>
2024-02-23 14:21:56 +0530

    valid paranthesis

--- src/DAY_2/problem/ValidParanthesis.java
+++ src/DAY_2/problem/ValidParanthesis.java
@@ -16,29 +16,95 @@ public class ValidParanthesis {
 //*Should always repay the loan you got
 //*you cannot repay a loan you don’t have
 //*you cannot get a loan not repay it.
-    public static void main(String[] args) {
-        String str = "((()))";
+
+    static void single_symbol(String str){
         ArrayDeque<Character> stack = new ArrayDeque<>();
         for(int i = 0;i<str.length();i++){
             char temp = str.charAt(i);
             if(temp == '('){
                 stack.push(temp);
             }else{ // )
-                // ( ( ) ) ) ( )
+                // ( ( ) ) )
                 if(stack.isEmpty()){
                     System.out.println("invalid");
-                    break;
+                    return;
                 }
                 stack.pop();
             }
         }
-
-
+        if(stack.isEmpty()){
+            System.out.println("valid");
+        }else{
+            System.out.println("invalid");
+        }
         System.out.println(stack);
+    }
+
+
+    static void multiplesymbols(String str){
+        // () {} []
+        // ()[]{{}
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        for(int i = 0;i<str.length();i++){
+            char temp = str.charAt(i);
+            if(temp == '(' || temp == '[' || temp == '{'){
+                stack.push(temp);
+            }else{ // ) ] }
+                // ( == )
+                // [ == ]
+                // { == }
+                if(stack.isEmpty()){
+                    System.out.println("invalid");
+                    return;
+                }else{
+                    switch (stack.peek()){
+                        case '(':
+                            if(temp == ')') {
+                                stack.pop();
+                            }else {
+                                System.out.println("invalid");
+                                return;
+                            }
+                            break;
+                        case '[':
+                            if(temp == ']')
+                                stack.pop();
+                            else{
+                                System.out.println("invalid");
+                                return;
+                            }
+                            break;
+                        case '{':
+                            if(temp == '}')
+                                stack.pop();
+                            else{
+                                System.out.println("invalid");
+                                return;
+                            }
+                            break;
+                    }
+                }
+
+            }
+        }
+        if(stack.isEmpty()){
+            System.out.println("valid");
+        }else{
+            System.out.println("invalid");
+        }
+        System.out.println(stack);
+    }
+
+    public static void main(String[] args) {
+//        String str = ")";
+        String str_multiple = "{[]}";
+//        single_symbol(str);
+        multiplesymbols(str_multiple);
 
     }
 
 
-
+    //    ( [ ) ]
+    //    ( [ ( { } ) ] )
 
 }
