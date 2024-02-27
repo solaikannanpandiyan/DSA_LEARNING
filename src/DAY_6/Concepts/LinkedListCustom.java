91d735ff2b6150645c3433589d433d838c295946
solaikannan pandiyan <solaikannanpandiyan@gmail.com>
2024-02-27 11:21:34 +0530

    linked list creation forward and backward

--- src/DAY_6/Concepts/LinkedListCustom.java
+++ src/DAY_6/Concepts/LinkedListCustom.java
@@ -3,6 +3,16 @@ package DAY_6.Concepts;
 
 import java.util.Arrays;
 
+
+class employee{
+//    String name;
+    int age;
+//    String dept;
+    int experience;
+    int salary;
+
+}
+
 class LinkedListNode{
 
     int data;
@@ -14,13 +24,34 @@ class LinkedListNode{
         this.next = next;
     }
 
-
-
 }
 public class LinkedListCustom {
 
-    static LinkedListNode createSinglyLinkedList(int[] input){
+    static LinkedListNode createSinglyLinkedListBackward(int[] input){
         LinkedListNode head = null;
+        LinkedListNode future = null;
+        for(int i = input.length-1;i>=0;i--){
+            head = new LinkedListNode(input[i],future);
+            future = head;
+        }
+
+        return head;
+    }
+
+    static LinkedListNode createSinglyLinkedListForward(int[] input){
+        LinkedListNode past = null;
+        LinkedListNode tail = null;
+        LinkedListNode head = null;
+        for(int i = 0;i<input.length;i++){
+            tail = new LinkedListNode(input[i],null);
+            if (past == null) { // will be true only once or first time
+                head = tail;
+                past = tail;
+                continue;
+            }
+            past.next = tail;
+            past = tail;
+        }
 
         return head;
     }
@@ -28,7 +59,7 @@ public class LinkedListCustom {
     public static void main(String[] args){
         int[] x = {1,5,7,9};
         System.out.println(Arrays.toString(x));
-//        LinkedListNode node4 = new LinkedListNode(9,null);
+        LinkedListNode node4 = new LinkedListNode(9,null);
 //        LinkedListNode node3 = new LinkedListNode(7,node4);
 //        LinkedListNode node2 = new LinkedListNode(5,node3);
 //        LinkedListNode node = new LinkedListNode(1,node2);
@@ -37,7 +68,7 @@ public class LinkedListCustom {
 //            System.out.println(node.data);
 //            node = node.next;
 //        }
-        LinkedListNode head = createSinglyLinkedList(x);
+        LinkedListNode head = createSinglyLinkedListForward(x);
         for(LinkedListNode temp = head;temp!=null;temp = temp.next){
             System.out.println(temp.data);
         }
