54b85a3c391b38e0f580bab13ce6ce722e1e33a3
solaikannan pandiyan <solaikannanpandiyan@gmail.com>
2024-02-27 10:02:33 +0530

    singly linked list

--- src/DAY_6/Concepts/LinkedListCustom.java
+++ src/DAY_6/Concepts/LinkedListCustom.java
@@ -1,7 +1,49 @@
 package DAY_6.Concepts;
 
+
+import java.util.Arrays;
+
+class LinkedListNode{
+
+    int data;
+
+    LinkedListNode next;
+
+    public LinkedListNode(int data, LinkedListNode next) {
+        this.data = data;
+        this.next = next;
+    }
+
+
+
+}
 public class LinkedListCustom {
 
-    public
+    static LinkedListNode createSinglyLinkedList(int[] input){
+        LinkedListNode head = null;
+
+        return head;
+    }
+
+    public static void main(String[] args){
+        int[] x = {1,5,7,9};
+        System.out.println(Arrays.toString(x));
+//        LinkedListNode node4 = new LinkedListNode(9,null);
+//        LinkedListNode node3 = new LinkedListNode(7,node4);
+//        LinkedListNode node2 = new LinkedListNode(5,node3);
+//        LinkedListNode node = new LinkedListNode(1,node2);
+//        LinkedListNode head = node;
+//        while(node != null){
+//            System.out.println(node.data);
+//            node = node.next;
+//        }
+        LinkedListNode head = createSinglyLinkedList(x);
+        for(LinkedListNode temp = head;temp!=null;temp = temp.next){
+            System.out.println(temp.data);
+        }
+
+    }
+
+
 
 }
