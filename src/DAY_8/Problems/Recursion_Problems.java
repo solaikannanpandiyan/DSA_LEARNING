49adc95ad1b0dff4eeeb5f42ae9ae8a7829613de
solaikannan pandiyan <solaikannanpandiyan@gmail.com>
2024-02-29 14:32:42 +0530

    deleting node using Recursion

--- src/DAY_8/Problems/Recursion_Problems.java
+++ src/DAY_8/Problems/Recursion_Problems.java
@@ -29,6 +29,37 @@ public class Recursion_Problems {
         printLL(head.next);
     }
 
+    static void printOneNode(Node temp){
+        String nextValue = temp.next==null?"null": String.valueOf(temp.next.hashCode());
+        System.out.println("LinkedListNode{" +
+                "id=" + temp.getId() +
+                ", data=" + temp.data +
+                ", next=" + nextValue +
+                '}');
+    }
+
+    static void printNodeChain(Node node){
+        for(Node temp = node; temp!=null; temp = temp.next){
+            String nextValue = temp.next==null?"null": String.valueOf(temp.next.hashCode());
+            System.out.println("LinkedListNode{" +
+                    "id=" + temp.getId() +
+                    ", data=" + temp.data +
+                    ", next=" + nextValue +
+                    '}');
+        }
+    }
+
+    static void deleteNode(Node prev ,Node head, int target){
+        if(head.data == target){
+            prev.next = head.next;
+            head.next = null;
+            System.out.println("removed node");
+            printOneNode(head);
+            return;}
+//        System.out.println(head.data);
+        deleteNode(head,head.next,target);
+    }
+
     static int fib(int x){
         if(x<=2){
             return x -1;
@@ -53,12 +84,25 @@ public class Recursion_Problems {
     }
 
     public static void main(String[] args){
-        int[] arr = {1,2,3,4,5};
+        int[] arr = {1,10,5,45,6};
         Node head = createSinglyLinkedListBackward(arr);
 //        printLL(head);
         //fibonacci series:  0 1 1 2 3 5 8 13 21 34
         //*nth fibonaci num: 1 2 3 4 5 6 7 8  9  10
-        int x = fibIter(500);
-        System.out.println(x);
+//        int x = fibIter(500);
+//        System.out.println(x);
+
+
+//        problem #1 Delete a linked list node with value 10 using recursion
+//        1 -> 10 -> 5 -> 45 -> 6 -> null
+//        1) Operation: delete (prev node)
+//        2) Terminal case: after we reach the required node
+//        3) function call: (curr,next)
+        int target = 10;
+        System.out.println("BEFORE");
+        printNodeChain(head);
+        deleteNode(null,head,target);
+        System.out.println("AFTER");
+        printNodeChain(head);
     }
 }
